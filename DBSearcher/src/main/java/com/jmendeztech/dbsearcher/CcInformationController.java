866f638430dd57579c705330b9da69f4b59a8b56
Administrator <Administrator@192.168.2.70>
2023-10-30 23:17:53 +0000

    not using system print out

--- DBSearcher/src/main/java/com/jmendeztech/dbsearcher/CcInformationController.java
+++ DBSearcher/src/main/java/com/jmendeztech/dbsearcher/CcInformationController.java
@@ -24,7 +24,7 @@ public class CcInformationController {
     @GetMapping
     public ResponseEntity<List<CcInformation>> getAllCcInformation() {
     	
-    	System.out.println("SEARCHING FOR DATA!!");
+    	
     	
         List<CcInformation> ccInformationList = ccInformationService.getAllCcInformation();
         return ResponseEntity.ok(ccInformationList);
